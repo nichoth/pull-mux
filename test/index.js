898fc47353ae7343592c906d06c2a818c90881e8
Nick Thomas <nichoth@gmail.com>
2017-01-09 15:08:43 -0800

    test custom mux

--- test/index.js
+++ test/index.js
@@ -25,3 +25,28 @@ test('create a namespaced stream from an object', function (t) {
         })
     )
 })
+
+
+test('pass in a mux function', function (t) {
+    t.plan(2)
+    var streams = {
+        a: S.values([1,2]),
+        b: S.values([3,4])
+    }
+    var stream = mux(streams, function muxer (type, ev) {
+        return { type: type, data: ev }
+    })
+
+    S(
+        stream,
+        S.collect(function (err, res) {
+            t.error(err)
+            t.deepEqual(res, [
+                { type: 'a', data: 1 },
+                { type: 'b', data: 3 },
+                { type: 'a', data: 2 },
+                { type: 'b', data: 4 },
+            ], 'should map the events with the function')
+        })
+    )
+})
